058ba057aa885a5acadb43cb673a32b8c4821fa9
Nicolly Ribeiro Luz <nicolly.luz@ufu.br>
2024-11-12 09:59:28 -0300

    Update Main.java

--- Lab 7/Questao02/src/Main.java
+++ Lab 7/Questao02/src/Main.java
@@ -1,3 +1,9 @@
+/*O padrão Template define uma estrutura base, um "esqueleto", para a classe pai, 
+e delega a implementação de outras funcionalidades para suas subclasses, enquanto que o padrão Decorator
+permite adicionar novas funcionalidades, comportamentos adicionais a objetos de forma dinâmica e flexível,
+sem alterar a classe base, e utilizando composição ao invés de herança.
+*/
+
 public class Main {
     public static void main(String[] args) {
        PagamentoTemplate pagOnline = new PagamentoOnline();
